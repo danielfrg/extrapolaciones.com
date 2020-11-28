d7fb3f2bdc9644c3ba1ce54f904c9f0ef895453c
Daniel Rodriguez <daniel@danielfrg.com>
2020-11-28 13:30:03 -0500

    mobile footer

--- python/articles.py
+++ python/articles.py
@@ -226,7 +226,7 @@ def video(block):
         params = parse_qs(url.query)
         id_ = params["v"][0]
         iframe = f'<iframe src="https://www.youtube.com/embed/{id_}" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border:0;" allowfullscreen="" title="YouTube Video"></iframe>'
-        return f'<div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">{iframe}</div>'
+        return f'<div class="notion-video" style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">{iframe}</div>'
     return ""
 
 
